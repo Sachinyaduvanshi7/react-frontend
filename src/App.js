9e6745d7821c8d9e20e5ced4eec2aa4af18c3bdd
Sachinyaduvanshi7 <sachinyrao7@gmail.com>
2024-10-07 17:30:35 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Home() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from AWS EKS</p>
+        <p>Hello from AWS EKS -1 </p>
       </header>
     </div>
   );
